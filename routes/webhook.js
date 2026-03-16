9276423ceb584ef1c01eb1e5e453d2c66a23c13f
Adarsh Shelore <134371739+adarshshelore@users.noreply.github.com>
2026-03-16 16:38:58 +0530

    Update webhook.js

--- routes/webhook.js
+++ routes/webhook.js
@@ -67,7 +67,7 @@ router.get("/", (req, res) => {
   const mode      = req.query["hub.mode"];
   const token     = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
-
+  console.log(mode, token, challenge);
   if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
     console.log("✅ Webhook verified");
     return res.status(200).send(challenge);
